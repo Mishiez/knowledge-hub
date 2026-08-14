7f3ba0b70876945dee9c40be1334c55e488488cd
Michelle Gathoni <michellegathonimain@gmail.com>
2026-08-14 12:57:09 +0300

    feat: add post_list and post_detail views with URLs and templates

---
+++ hub/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = "hub"
+
+urlpatterns = [
+    path("", views.post_list, name="post_list"),
+    path("posts/<slug:slug>/", views.post_detail, name="post_detail"),
+]
