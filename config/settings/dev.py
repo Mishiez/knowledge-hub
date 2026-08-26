b7a111e78860a4be2f11427a9b69d4128e4449fe
Michelle Gathoni <michellegathonimain@gmail.com>
2026-08-26 22:46:00 +0300

    chore: configure DRF and CORS for frontend connection

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -4,3 +4,8 @@ import os
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
