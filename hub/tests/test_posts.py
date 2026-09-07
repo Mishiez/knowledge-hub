5f0f7cb968faf2c63726835a3e05be4fd45b8c5c
Michelle Gathoni <michellegathonimain@gmail.com>
2026-09-07 12:10:53 +0300

    Revert "test: intentionally break a test to verify pipeline blocks build/publish (E2 evidence)"
    This reverts commit e10af64bb4e330f9bfed8a08f09309322eded8e4.

--- hub/tests/test_posts.py
+++ hub/tests/test_posts.py
@@ -24,7 +24,7 @@ class PostAPITests(APITestCase):
     def test_retrieve_post_public(self):
         response = self.client.get(self.detail_url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data['title'], 'This will defginitely fail')
+        self.assertEqual(response.data['title'], 'Original Title')
 
     def test_retrieve_nonexistent_post_404(self):
         url = reverse('api-post-detail', kwargs={'slug': 'does-not-exist'})
